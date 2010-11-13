9f84bad1738e42fe14f84d59ae7db400a36a334a
Alexander Surma <surma@78762.de>
2010-11-13 23:20:34 +0100

    Implemented actual algorithm routine

--- algorithm.go
+++ algorithm.go
@@ -11,10 +11,42 @@ package genetics
 // result in a runnable genetic algorithm
 type Algorithm struct {
 	PopulationSize int
-	population     []Subject
+	GenomeLength   int
+	population     Population
 	Initializer    Initializer
 	Evaluator      Evaluator
 	Selector       Selector
 	Breeder        Breeder
 	Mutator        Mutator
+}
+
+// Starts a new population of size PopulationSize
+// and inits every subject using the Initializer
+func (a *Algorithm) Init() {
+	a.population = make(Population, a.PopulationSize)
+	for i := range a.population {
+		a.population[i] = Subject{Genome: a.Initializer.NewGenome(a.GenomeLength)}
+	}
+	a.evaluateAll()
+}
+
+// Creates the next generation
+// At this point, the populations has to be evaluated already
+func (a *Algorithm) Next() {
+	newpop := a.Breeder.Breed(a.population, a.Selector)
+	for i := range newpop {
+		newpop[i] = a.Mutator.Mutate(newpop[i])
+	}
+	a.population = newpop
+}
+
+// Returns the current population
+func (a *Algorithm) Population() Population {
+	return a.population
+}
+
+func (a *Algorithm) evaluateAll() {
+	for i := range a.population {
+		a.Evaluator.Evaluate(a.population[i])
+	}
 }
