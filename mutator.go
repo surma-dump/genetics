416446248a60ac336414886a3c9e9f26f0e5da55
Alexander Surma <surma@78762.de>
2010-11-14 00:58:14 +0100

    Added RandomMutator

--- mutator.go
+++ mutator.go
@@ -1,5 +1,9 @@
 package genetics
 
+import (
+	"rand"
+)
+
 // A mutator has the chance of mutating the
 // subjects of a new generation
 type Mutator interface {
@@ -13,6 +17,29 @@ func NewNopMutator() Mutator {
 	return new(NopMutator)
 }
 
-func (n *NopMutator) Mutate(s Subject) Subject {
+func (m *NopMutator) Mutate(s Subject) Subject {
+	return s
+}
+
+
+// Resets each value in the genome
+// with a fixed probability
+type RandomMutator struct {
+	prob float64
+}
+
+func NewRandomMutator(prob float64) Mutator {
+	m := new(RandomMutator)
+	m.prob = prob
+	return m
+}
+
+func (m *RandomMutator) Mutate(s Subject) Subject {
+	for i := range s.Genome {
+		v := rand.Float64()
+		if v < m.prob {
+			s.Genome[i] = rand.Float64()
+		}
+	}
 	return s
 }
